163d6bd97786319e28086b0c4d20e820d5c88487
jorgecpp <jorgetevescpp@gmail.com>
2026-06-16 10:02:51 -0500

    implementando los idiomas para el portafolio

--- src/app/[locale]/(routes)/page.tsx
+++ src/app/[locale]/(routes)/page.tsx
@@ -1,24 +1,20 @@
 import { AboutMe } from "@/src/components/aboutMe/AboutMe";
 import { ContactMe } from "@/src/components/contactMe/ContactMe";
-import { Footer } from "@/src/components/footer/Footer";
 import { Hero } from "@/src/components/hero/Hero";
 import { MyProjects } from "@/src/components/myProjects/MyProjects";
-import { Navbar } from "@/src/components/navbar/navbar";
 import { ProfessionalExperience } from "@/src/components/professionalExperience/ProfessionalExperience";
 import { Tools } from "@/src/components/tools/Tools";
 
 
 export default function Home() {
   return (
-    <main>
-      <Navbar/>
+    <main> 
       <Hero/>
       <AboutMe/>
       <ProfessionalExperience/>
       <MyProjects/>
       <Tools/>
       <ContactMe/>
-      <Footer/>
     </main>
   );
 }
